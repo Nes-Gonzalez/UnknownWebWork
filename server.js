a793002d6bdc9680d3b83f0fd4b2c48e18c441de
Nestor <n_gonzalez@live.com>
2018-05-26 01:40:33 -0500

    added correct post build file path

--- server.js
+++ server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static(__dirname + '/dist/UnknownWeb'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join('/dist/index.html'));
+app.get('/*all', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/UnknownWeb/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
